8fa5ef92464e45d1832a4cbec8b38d2ea01255ac
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-07-27 18:32:03 -0500

    se creo el componente griditem el cual recibe en sus props cada propuedad de los gifs y mediante desestructuracion se acomoda el contenido en cards de bootstrap

--- src/components/GifGrid/GifGrid.js
+++ src/components/GifGrid/GifGrid.js
@@ -1,13 +1,16 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import { GridItem } from "../GridItem/GridItem";
 
 export const GifGrid = ({category}) => {
+
+    const [gif, setGif] = useState([]);
 
     useEffect(() => {
         getGifs();
     }, []);
 
     const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=SSU4rxb9RPFUuGbxhXDXrS8VAcPmrDo4&limit=5`;
+        const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=SSU4rxb9RPFUuGbxhXDXrS8VAcPmrDo4&limit=6`;
         const respuesta = await fetch(url);
         const {data} = await respuesta.json();
 
@@ -15,16 +18,24 @@ export const GifGrid = ({category}) => {
             return {
                 id: gif.id,
                 title: gif.title,
-                url: gif.images?.downsized_medium.url
+                url: gif.images?.downsized_medium.url,
+                date: gif.import_datetime
             }
         })
 
-        console.log(gifs)
+        setGif(gifs)
     }
 
     return (
         <>
-            <h5>{category}</h5>
+            <h3 className="text-center mt-5 text-uppercase">{category}</h3>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {
+                    gif.map(item => {
+                        return <GridItem key={item.id} {...item} />
+                    })
+                }
+            </div>
         </>
     )
 }
---
+++ src/components/GridItem/GridItem.js
@@ -0,0 +1,16 @@
+export const GridItem = ({title, url, date}) => {
+    return (
+        <div className="col">
+            <div className="card h-100">
+                <img src={url} className="card-img-top" alt={url}/>
+                <div className="card-body">
+                    <h5 className="card-title">{title || 'Sin titulo'}</h5>
+                    <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Ver imagen</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Fecha y hora de subida: {date}</small>
+                </div>
+            </div>
+        </div>
+    )
+}
