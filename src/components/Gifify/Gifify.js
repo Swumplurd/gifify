9b104b38651a25cfdc60f00622632847e8599abc
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-07-27 14:46:47 -0500

    se agrego useState para agregar categorias y un botton que dispara el evento
    se instalo bootstrap5, pero no ha sido utilizado aun

--- src/components/Gifify/Gifify.js
+++ src/components/Gifify/Gifify.js
@@ -1,8 +1,28 @@
+import { useState } from 'react'
+
 export const Gifify = () => {
+    const [categories, setCategories] = useState(['Demon Slayer', 'Rick and Morty', 'Ghibli']);
+
+    const handleAdd = () => {
+        setCategories(['Hola', ...categories])
+    }
+
     return (
         <>
             <h2>Gifify</h2>
             <hr/>
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return (
+                            <li key={category}>{category}</li>
+                        )
+                    })
+                }
+            </ol>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Gifify } from './components/Gifify/Gifify';
 
+import './index.css';
+
 ReactDOM.render(
   <Gifify />,
   document.querySelector('#root')
