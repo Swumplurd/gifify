bfe80860f24f33a8041638eade192c1c762c577d
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-07-27 20:11:25 -0500

    update de jumbotron en celulares

--- src/components/Gifify/Gifify.js
+++ src/components/Gifify/Gifify.js
@@ -7,7 +7,7 @@ export const Gifify = () => {
 
     return (
         <>
-            <div className="p-5 my-4 bg-dark bg-gradient shadow-lg text-light rounded-3">
+            <div className="p-5 my-4 bg-dark bg-gradient shadow-lg text-light rounded-3 d-none d-sm-block">
                 <div className="container-fluid py-5">
                     <h1 className="display-5 fw-bold">Gifify</h1>
                     <p className="col-md-8 fs-4">Generamos peticiones http a la API de GIPHY mediante el input situado debajo... para mas informacion acerca de la API dar click en el siguiente boton...</p>
