8687ae417ed315b5b3bf55c75a352840f41634a2
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-07-27 19:31:52 -0500

    se creo un custom hook el cual permite que nuestro gifgrid sea mas limpio
    el custom hook se engarga de hacer el useeffect y generar la peticion http
    
    al obtener la data hace el setState

--- src/components/GifGrid/GifGrid.js
+++ src/components/GifGrid/GifGrid.js
@@ -1,19 +1,14 @@
-import {useEffect, useState} from "react";
-import { getGifs } from "../../helpers/getGifs";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
 import { GridItem } from "../GridItem/GridItem";
 
 export const GifGrid = ({category}) => {
 
-    const [gif, setGif] = useState([]);
-
-    useEffect(() => {
-        getGifs(category)
-            .then( gifs => setGif(gifs))
-    }, [category]);
+    const {data:gif, loading} = useFetchGifs(category);
 
     return (
         <>
             <h3 className="text-center mt-5 text-uppercase">{category}</h3>
+            {loading && <p>Cargando...</p>}
             <div className="row row-cols-1 row-cols-md-3 g-4">
                 {
                     gif.map(item => {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Gifify } from './components/Gifify/Gifify';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import 'animate.css/animate.css';
 
 ReactDOM.render(
   <div className="container">
