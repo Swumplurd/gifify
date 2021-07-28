28ca71985813bc9d2f1c253144286506b980d902
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-07-27 20:29:35 -0500

    actualizacion de vista en celulares

--- src/components/Gifify/Gifify.js
+++ src/components/Gifify/Gifify.js
@@ -7,11 +7,11 @@ export const Gifify = () => {
 
     return (
         <>
-            <div className="p-5 my-4 bg-dark bg-gradient shadow-lg text-light rounded-3 d-none d-sm-block">
+            <div className="p-5 my-4 bg-dark bg-gradient shadow-lg text-light rounded-3">
                 <div className="container-fluid py-5">
                     <h1 className="display-5 fw-bold">Gifify</h1>
-                    <p className="col-md-8 fs-4">Generamos peticiones http a la API de GIPHY mediante el input situado debajo... para mas informacion acerca de la API dar click en el siguiente boton...</p>
-                    <a className="btn btn-primary btn-lg" target="_blank" rel="noopener noreferrer" href="https://developers.giphy.com/">API GIPHY</a>
+                    <p className="col-md-8 fs-4 d-none d-sm-block">Generamos peticiones http a la API de GIPHY mediante el input situado debajo... para mas informacion acerca de la API dar click en el siguiente boton...</p>
+                    <a className="btn btn-primary btn-lg d-none d-sm-block" target="_blank" rel="noopener noreferrer" href="https://developers.giphy.com/">API GIPHY</a>
                 </div>
             </div>
             <AddCategory setCategories={setCategories}/>
